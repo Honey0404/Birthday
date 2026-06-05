fbec91d2fd589a2d48256aec66b9ddbfb4439f0f
Honey0404 <camarjeet49@gmail.com>
2026-06-05 20:13:20 +0530

    Update typing.js

--- js/typing.js
+++ js/typing.js
@@ -1,15 +1,8 @@
 const msg = `Happy Birthday, My Love ❤️
-
 Eight months may not seem like a long time to the world, but to me, they have been the most beautiful months of my life. Somewhere between our conversations, our laughter, and the countless moments we've shared, you became more than just a part of my life—you became my home.
-
-You are the first thought that makes me smile in the morning and the last thought that comforts me at night. You have brought so much happiness, warmth, and meaning into my world that I cannot imagine it without you.
-
+You are the first thought that makes me smile in the morning and the last thought that comforts me at night.
 I love you more than words will ever be able to explain, and with every passing day, that love only grows stronger.
-
 Happy Birthday, my beautiful girl.
-
-Forever yours ❤️
-
 Love You Forever ❤️`;
 
 function startTyping() {
