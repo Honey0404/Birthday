4e53e1e2c03cb0699364c539837cb1a1242de7ad
Honey0404 <camarjeet49@gmail.com>
2026-06-05 20:03:23 +0530

    Update typing.js

--- js/typing.js
+++ js/typing.js
@@ -4,10 +4,6 @@ Eight months may not seem like a long time to the world, but to me, they have be
 
 You are the first thought that makes me smile in the morning and the last thought that comforts me at night. You have brought so much happiness, warmth, and meaning into my world that I cannot imagine it without you.
 
-If I could give you one gift today, it would be the ability to see yourself through my eyes. Then you would know how deeply you are loved, how precious you are, and how grateful I am for every moment with you.
-
-On your birthday, I don't just celebrate the day you were born—I celebrate the day the world was blessed with someone who would one day become my greatest blessing.
-
 I love you more than words will ever be able to explain, and with every passing day, that love only grows stronger.
 
 Happy Birthday, my beautiful girl.
